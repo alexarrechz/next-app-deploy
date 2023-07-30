eee19839a4b4db68766d97e7639154ab7d6dbb4f
alexarrechz <ivanalejandroarreolachavez@gmail.com>
2023-07-30 14:18:32 -0600

    2.9

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+  // Add your specific Next.js configurations here
+  // For example, you can enable the 'images' experimental feature:
+  experimental: {
+    images: true,
+  },
+
+  // Replace the App Router with static HTML export
+  // This enables static HTML export instead of the App Router
+  output: {
+    // Set the output directory for the exported files
+    path: '/path/to/your/export/folder',
+    // (Optional) Set a prefix for all exported paths
+    // For example, '/your-app-prefix' will result in '/your-app-prefix/index.html' for the homepage
+    publicPath: '/your-app-prefix',
+  },
+};
